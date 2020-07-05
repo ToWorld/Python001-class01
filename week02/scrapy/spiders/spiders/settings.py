e95d10a90659e715779b57d9217f401450b1b4a5
ToWorld <jayzhu1215@gmail.com>
2020-07-05 23:19:12 +0800

    更新week02作业
    增加代理IP功能,抓取数据写mysql,增加异常处理
    
    更新week02学习笔记

--- week02/scrapy/spiders/spiders/settings.py
+++ week02/scrapy/spiders/spiders/settings.py
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'spiders.middlewares.SpidersDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'spiders.middlewares.SpidersDownloaderMiddleware': 543,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
